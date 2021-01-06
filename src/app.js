1cb63ff8c46f4f927ae48bcede86be79e04848ed
kasozi wilson <soziandwelewilson@gmail.com>
2021-01-06 00:22:04 -0500

    recieve multipart form data

--- src/app.js
+++ src/app.js
@@ -17,13 +17,7 @@ const corsMod = cors({
   origin: config.clientDomain,
   credentials: true,
   methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
-  allowedHeaders: [
-    'Content-Type',
-    'origin',
-    'Authorization',
-    'boundary',
-    'Boundary',
-  ],
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
 });
 
 app.use(corsMod);
