2637b2b017bd7acc2e3337193ca7e3c9a9837b03
kasozi wilson <soziandwelewilson@gmail.com>
2021-01-09 19:03:45 -0500

    able to save image on google cloud storage

--- src/controller/ImageController.js
+++ src/controller/ImageController.js
@@ -1,7 +1,18 @@
-module.exports = {
-  newImage(req, res) {
-    console.log(req.file);
-    console.log(req.body);
-    res.status('202').send({});
-  },
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage({
+  projectId: process.env.GCS_PROJECT_ID,
+  credentials: JSON.parse(process.env.GCS_KEY_FILE),
+});
+const myBucket = storage.bucket(process.env.GCS_BUCKET_NAME);
+
+module.exports.newImage = function(req, res) {
+  console.log(req.file);
+  const file = myBucket.file(req.file.originalname);
+
+  file.save(req.file.buffer, function(err) {
+    if (!err) {
+      console.log('SUCCESS SENT IMAGE');
+      res.status('202').send({});
+    }
+  });
 };
