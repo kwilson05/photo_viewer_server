9f64b8ece70a56bb47db7696c7935d291381d804
kasozi wilson <soziandwelewilson@gmail.com>
2021-02-04 22:57:31 -0500

    create image skeleton

--- src/controller/ImageController.js
+++ src/controller/ImageController.js
@@ -2,22 +2,19 @@ const ImageCloudStorage = require('../models/ImageCloudStorage');
 const ImageFileDbo = require('../models/ImageFileDbo');
 module.exports.newImage = async function(req, res) {
   try {
-    await ImageCloudStorage.saveBinaryImage(
+    const imageDetails = JSON.parse(req.body.imageDetails);
+
+    imageDetails.filePath = req.file.originalname;
+
+    const newImage = await ImageFileDbo.create(imageDetails);
+    const cloudResponse = await ImageCloudStorage.saveBinaryImage(
       req.file.originalname,
       req.file.buffer
     );
-
-    const imageDetails = JSON.parse(req.body.imageDetails);
-
-    const imageFile = {
-      title: imageDetails.title,
-      description: imageDetails.description,
-      photoTakeDate: imageDetails.photoTakeDate,
-      filePath: req.file.originalname,
-    };
-    ImageFileDbo.create(imageFile);
-    res.status(200).send({});
+    console.log(cloudResponse);
+    res.status(200).send(newImage);
   } catch (err) {
+    console.log(err);
     res.status(501).send({
       error: 'Could not save image',
     });
--- src/models/ImageFileDbo.js
+++ src/models/ImageFileDbo.js
@@ -2,11 +2,12 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 module.exports.create = async function(imageFile) {
+  console.log(imageFile);
   return await prisma.imagefile.create({
     data: {
       filePath: imageFile.filePath,
       title: imageFile.title,
-      photoTakeDate: imageFile.photoTakenDate,
+      photoTakenDate: new Date(imageFile.photoTakenDate),
       description: imageFile.description,
     },
   });
